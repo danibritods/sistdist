d709d5d72496e1bda0d33c91b616c542d514ab14
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2022-09-29 12:56:05 -0300

    feat: restarting redisrpc

--- redis/redisrpc/server.py
+++
@@ -1,7 +0,0 @@
-'''assas'''
-import redisrpc as redis
-redis_server = redis.Redis()
-message_queue = 'calc'
-local_object = calc.Calculator()
-server = redisrpc.Server(redis_server, message_queue, local_object)
-server.run()
