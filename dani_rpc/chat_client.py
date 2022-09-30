e8ee4e5a26506db3bdaf307e6f4808776db155b7
Daniel Brito <danielbrito@pq.uenf.br>
2022-09-30 20:27:28 -0300

    feat: client interface

--- dani_rpc/chat_client.py
+++ dani_rpc/chat_client.py
@@ -12,9 +12,22 @@ def test_routine():
     joao.send_msg("Daniel","Te pago amanhã")
     print(doris.check_msg())
 
+def client_interface():
+    rpc_client = rpc.Client()
+    client_id = input("Write your client ID: ")
+    client = chat.Client(client_id, rpc_client.connection)
+    message = ""
+
+    while message != "q!":
+        receiver_id = input("receiver_id: ")
+        message = input("message: ")
+
+        client.send_msg(receiver_id, message)
+
+        print(f"{10*'-·-'} \n {client.check_msg()} \n {15*'-·'}")
 
 if __name__ == "__main__":
-    test_routine() 
+    client_interface() 
 
 
 
